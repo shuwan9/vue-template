3d9f0cc76a063e0880142d3ce63ec61817651f1c
shuwang-vue <1150569362@qq.com>
2019-06-13 10:39:54 +0800

    remove muse-ui

--- src/install.js
+++ src/install.js
@@ -3,14 +3,11 @@ import Vue from "vue"
 import Router from "vue-router"
 import Vuex from "vuex"
 
-import MuseUI from "muse-ui"
-
 import Ls from "@/plugins/LocalStorage"
 
 const install = () => {
   Vue.use(Router)
   Vue.use(Vuex)
-  Vue.use(MuseUI)
   Vue.use(Ls)
   return Vue
 }
